37ded3ec4c640402507329f1fe77b0f7881978f8
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-05-27 14:54:26 -0500

    chore: rename main class and test

--- src/main/java/com/pragma/powerup/PlazoletaUsersServiceApplication.java
+++ src/main/java/com/pragma/powerup/PlazoletaUsersServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PowerUpApplication {
+public class PlazoletaUsersServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PowerUpApplication.class, args);
+		SpringApplication.run(PlazoletaUsersServiceApplication.class, args);
 	}
 
 }
--- src/test/java/com/pragma/powerup/PlazoletaUsersServiceApplicationTests.java
+++ src/test/java/com/pragma/powerup/PlazoletaUsersServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PowerUpApplicationTests {
+class PlazoletaUsersServiceApplicationTests {
 
     @Test
     void contextLoads() {
