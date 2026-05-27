0ab626ed5432acff55034c063a0327e080a5c45f
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-05-27 17:38:44 -0500

    feat(users): implement create user owner
    All test pass

--- src/main/java/com/pragma/plazoleta/domain/usecase/UserUseCase.java
+++ src/main/java/com/pragma/plazoleta/domain/usecase/UserUseCase.java
@@ -1,11 +1,68 @@
 package com.pragma.plazoleta.domain.usecase;
 
 import com.pragma.plazoleta.domain.api.IUserServicePort;
+import com.pragma.plazoleta.domain.enums.Roles;
+import com.pragma.plazoleta.domain.exception.user.DocumentNumberAlreadyExistsException;
+import com.pragma.plazoleta.domain.exception.user.EmailAlreadyExistsException;
+import com.pragma.plazoleta.domain.exception.user.RoleNotFoundException;
+import com.pragma.plazoleta.domain.exception.user.UserNotOfLegalAgeException;
+import com.pragma.plazoleta.domain.model.Role;
 import com.pragma.plazoleta.domain.model.User;
+import com.pragma.plazoleta.domain.spi.IPasswordEncoderPort;
+import com.pragma.plazoleta.domain.spi.IRolePersistencePort;
+import com.pragma.plazoleta.domain.spi.IUserPersistencePort;
+import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.Period;
+
+@RequiredArgsConstructor
 public class UserUseCase implements IUserServicePort {
+
+    private final IUserPersistencePort userPersistencePort;
+    private final IRolePersistencePort rolePersistencePort;
+    private final IPasswordEncoderPort passwordEncoderPort;
+
     @Override
     public User createOwner(User user) {
-        return null;
+        validateLegalAge(user.getBirthDate());
+        validateEmailUniqueness(user.getEmail());
+        validateDocumentNumberUniqueness(user.getDocumentNumber());
+
+        var ownerRole = resolveOwnerRole();
+        user.setRole(ownerRole);
+        user.setPassword(passwordEncoderPort.encode(user.getPassword()));
+
+        return userPersistencePort.save(user);
+    }
+
+    private void validateLegalAge(LocalDate birthDate) {
+        var age = Period.between(birthDate, LocalDate.now()).getYears();
+
+        if (age < User.MINIMUM_AGE) {
+            throw new UserNotOfLegalAgeException();
+        }
+    }
+
+    private void validateEmailUniqueness(String email) {
+        if (userPersistencePort.existsByEmail(email)) {
+            throw new EmailAlreadyExistsException();
+        }
+    }
+
+    private void validateDocumentNumberUniqueness(String documentNumber) {
+        if (userPersistencePort.existsByDocumentNumber(documentNumber)) {
+            throw new DocumentNumberAlreadyExistsException();
+        }
+    }
+
+    private Role resolveOwnerRole() {
+        var role = rolePersistencePort.findByName(Roles.OWNER.getName());
+
+        if (role == null) {
+            throw new RoleNotFoundException(Roles.OWNER.getId());
+        }
+
+        return role;
     }
 }
