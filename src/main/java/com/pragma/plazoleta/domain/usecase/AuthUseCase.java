968af6005d29a0aa056979b523313b36216db97a
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-05-29 14:45:04 -0500

    feat(auth): implement login in auth use case

--- src/main/java/com/pragma/plazoleta/domain/usecase/AuthUseCase.java
+++ src/main/java/com/pragma/plazoleta/domain/usecase/AuthUseCase.java
@@ -1,6 +1,8 @@
 package com.pragma.plazoleta.domain.usecase;
 
 import com.pragma.plazoleta.domain.api.IAuthServicePort;
+import com.pragma.plazoleta.domain.exception.auth.InvalidCredentialsException;
+import com.pragma.plazoleta.domain.model.User;
 import com.pragma.plazoleta.domain.spi.IPasswordEncoderPort;
 import com.pragma.plazoleta.domain.spi.ITokenServicePort;
 import com.pragma.plazoleta.domain.spi.IUserPersistencePort;
@@ -15,6 +17,26 @@ public class AuthUseCase implements IAuthServicePort {
 
     @Override
     public String login(String email, String rawPassword) {
-        return "";
+        var user = resolveUserByEmail(email);
+        validatePasswordMatch(rawPassword, user.getPassword());
+
+        return tokenServicePort.generateToken(user);
+    }
+
+    private User resolveUserByEmail(String email) {
+        var user = userPersistencePort.findByEmail(email);
+        if (user == null) {
+            throw new InvalidCredentialsException();
+        }
+
+        return user;
+    }
+
+    private void validatePasswordMatch(String rawPassword, String encryptedPassword) {
+        var isCorrectPassword = passwordEncoderPort.matches(rawPassword, encryptedPassword);
+
+        if (Boolean.FALSE.equals(isCorrectPassword)) {
+            throw new InvalidCredentialsException();
+        }
     }
 }
