05741a8e4315fd4d7c4d32febb6b2116be71ea19
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-05-29 18:52:24 -0500

    refactor(roles): use constants in roles enum

--- src/main/java/com/pragma/plazoleta/domain/enums/Roles.java
+++ src/main/java/com/pragma/plazoleta/domain/enums/Roles.java
@@ -1,18 +1,21 @@
 package com.pragma.plazoleta.domain.enums;
 
-import lombok.AllArgsConstructor;
+import com.pragma.plazoleta.domain.constants.RoleConstants;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public enum Roles {
 
-    ADMIN(1L, "ADMIN"),
-    OWNER(2L, "OWNER"),
-    EMPLOYEE(3L, "EMPLOYEE"),
-    CLIENT(4L, "CLIENT");
+    ADMIN(1L, RoleConstants.ROLE_ADMIN),
+    OWNER(2L, RoleConstants.ROLE_OWNER),
+    EMPLOYEE(3L, RoleConstants.ROLE_EMPLOYEE),
+    CLIENT(4L, RoleConstants.ROLE_CLIENT);
 
     private final Long id;
     private final String name;
 
+    Roles(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
